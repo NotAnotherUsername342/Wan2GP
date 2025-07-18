16468870c1fc49ab43271c555a94fe23c19fa4f9
NotAnotherUsername342 <121112787+NotAnotherUsername342@users.noreply.github.com>
2025-07-18 10:21:36 +0200

    Update and rename handler.py to 2

--- handler.py
+++
@@ -1,9 +0,0 @@
-import runpod
-from wgp import main_function  # <- Passe ggf. an, je nachdem wie du starten willst
-
-def handler(job):
-    input_data = job["input"]
-    result = main_function(input_data)
-    return result
-
-runpod.serverless.start({"handler": handler})
